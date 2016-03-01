0cc886d35760a07ff43ec522eed4d44b85a0a9dd
YAT <yat@yat-net.com>
2016-03-01 13:22:07 +0900

    v2.0.1

--- dev/js/config.js
+++ dev/js/config.js
@@ -1,18 +1,5 @@
 jQuery(function($) {
-	// $(".lout-base").onepage_scroll({
-	// 	sectionContainer: "section",
-	// 	easing: "ease",
-	// 	animationTime: 1000,
-	// 	pagination: true,
-	// 	updateURL: false,
-	// 	$.fn.moveTo:
-	// 	beforeMove: function(index) {},
-	// 	afterMove: function(index) {},
-	// 	loop: true,
-	// 	keyboard: true,
-	// 	responsiveFallback: false,
-	// 	direction: "vertical"
-	// });
+	//fullpage.js config
 	$('#fullpage').fullpage({
 		menu: '.lout-sidebar-navi',
 		anchors:['js-mainvisual', 'js-idea', 'js-overview', 'js-form', 'js-privacy']
